51fd3e5368219d5f43efb0377ec0d70731d8ff8a
Christian Areas <me@areas.me>
2022-12-11 17:28:20 -0800

    Cleaned up index.js and deleted stuff in output folder.

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-// Requirements:
-// https://courses.bootcampspot.com/courses/2188/assignments/38676?module_item_id=748551
-//
-// AS A developer
-// I WANT a README generator
-// SO THAT I can quickly create a professional README for a new project
-//
-// GIVEN a command-line application that accepts user input:
-// [x] WHEN I am prompted for information about my application repository
-//     THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// [x] WHEN I enter my project title
-//     THEN this is displayed as the title of the README
-// [x] WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-//     THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// [x] WHEN I choose a license for my application from a list of options
-//     THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// [x] WHEN I enter my GitHub username
-//     THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// [x] WHEN I enter my email address
-//     THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// [x] WHEN I click on the links in the Table of Contents
-//     THEN I am taken to the corresponding section of the README
+// Requirements: https://courses.bootcampspot.com/courses/2188/assignments/38676?module_item_id=748551.
 
-const fs = require ("fs")
 const inquire = require ("inquirer")
+const fs = require ("fs")
 
 // Prompts.
 const prompts = [
